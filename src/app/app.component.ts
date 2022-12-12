d2ecd8ff393268b5e6bec2f08fefc39a40099f7a
Carolista <cjones.renaissancewoman@gmail.com>
2022-12-12 09:45:10 -0600

    added ngFor example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  name: string,
+  description: string,
+  price: number,
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  itemsForSale: Item[] = [
+    { name: 'Pencils', description: 'a box of 12 basic #2 pencils', price: 2.49 },
+    { name: 'Crayons', description: 'a box of 48 crayons with sharpener', price: 4.99 },
+    { name: 'Erasers', description: 'a package of 3 large pink erasers', price: 1.29 },
+  ];
 
 }
